883dbf85c878f74c7454a4bff8cc7121525b4c66
Olivier Refalo <orefalo@snapcore.com>
2024-04-25 16:25:22 +0400

    ch

--- chatgpt-superset.js
+++ chatgpt-superset.js
@@ -40,7 +40,8 @@ const model = "gpt-4";
 async function rewrite(input, options) {
   openai.defaults.headers.common.Authorization = `Bearer ${options.apikey}`;
   const content =
-    "I want you to act as an English spelling corrector. I want you to only reply the correction, the improvements and nothing else, do not write explanations. Correct the following sentence: \n\n" + input.text.trim();
+    "I want you to act as an English spelling corrector, only reply the correction, the improvements and nothing else, do not write explanations. improve the following sentence using BBC english: \n\n" +
+    input.text.trim();
   const messages = [{ role: "user", content: content }];
   const { data } = await openai.post("chat/completions", {
     model: model,
@@ -59,7 +60,7 @@ async function rewrite(input, options) {
 async function summarize(input, options) {
   openai.defaults.headers.common.Authorization = `Bearer ${options.apikey}`;
   const content =
-    "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following text and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. Please avoid unnecessary details or tangential points: \n\n" +
+    "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following text and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. avoid unnecessary details or tangential points: \n\n" +
     input.text.trim();
   const messages = [{ role: "user", content: content }];
   const { data } = await openai.post("chat/completions", {
@@ -87,10 +88,10 @@ async function correctFrench(input, options) {
   return null;
 }
 
-async function correctGrammar(input, options) {
+async function improveText(input, options) {
   openai.defaults.headers.common.Authorization = `Bearer ${options.apikey}`;
   const content =
-    "I want you to act as an English spelling corrector and improver. Keep the meaning the same, but make them more business friendly. I want you to only reply the correction, the improvements and nothing else, do not write explanations. Correct and improve the following sentence: \n\n" +
+    "I want you to act as an English spelling corrector and improver. Keep the meaning the same, but make them more business friendly, avoid complex words and verbs. I want you to only reply the correction, the improvements and nothing else, do not write explanations. Correct and improve the following sentence: \n\n" +
     input.text.trim();
   const messages = [{ role: "user", content: content }];
   const { data } = await openai.post("chat/completions", {
@@ -118,7 +119,7 @@ exports.actions = [
   {
     title: "Improve the text",
     //after: "copy-result",
-    code: correctGrammar,
+    code: improveText,
     icon: "./spell-check.svg",
   },
   {
