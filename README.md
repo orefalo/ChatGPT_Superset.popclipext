0e675bc5f8c240ad09433bd80472fd51220277a7
Olivier Refalo <orefalo@snapcore.com>
2024-10-08 19:09:02 +0400

    cleaning up code and adding new actions

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # ChatGPT_Superset.popclipext
 
-This is my own CharGPT extension for popclip on OSX.
-Ez to customize, just edit the code and the js file
+This is my own CharGPT extension for PopClip
 
-1. clone the repo
-2. double click on the project should trigger extension import
+Simple to adjust; just edit the code and JavaScript file.
+
+## Install
+
+1. Clone the repo  ** git clone <repo url> **
+2. Double click on the toplevel folder with the **.popclipext**
+3. Follow PopClip instructions
+
+## Debug
+
+1. run **defaults write com.pilotmoon.popclip EnableExtensionDebug -bool YES**
+2. then open **console.app** and apply filter **Process:PopClip Category:Extension**
--- chatgpt-superset.js
+++ chatgpt-superset.js
@@ -1,52 +1,8 @@
-// #popclip extension for ChatGPT
-// name: ChatGPT Quick Actions
-// icon: iconify:logos:openai-icon
-// language: javascript
-// module: true
-// entitlements: [network]
-// options: [{
-//   identifier: apikey, label: API Key, type: string,
-//   description: 'Obtain API key from https://platform.openai.com/account/api-keys'
-// }]
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.actions = void 0;
-
 const openai = require("axios").create({
   baseURL: "https://api.openai.com/v1/",
 });
 
-const model = "gpt-4o";
-
-// async function prompt(input, options) {
-//   openai.defaults.headers.common.Authorization = `Bearer ${options.apikey}`;
-//   const content = input.text.trim();
-//   const messages = [{ role: "user", content: content }];
-//   const { data } = await openai.post("chat/completions", {
-//     model: model,
-//     messages,
-//   });
-//   const response = data.choices[0].message.content.trim();
-//   // if holding shift, copy just the response. else, paste the last input and response.
-//   if (popclip.modifiers.shift) {
-//     popclip.copyText(response);
-//   } else {
-//     popclip.pasteText(response);
-//   }
-//   return null;
-// }
-
-async function rewrite(input, options) {
-  openai.defaults.headers.common.Authorization = `Bearer ${options.apikey}`;
-  const content =
-    "I want you to act as an English spelling corrector, only reply the correction, the improvements and nothing else, do not write explanations. improve the following sentence using BBC english: \n\n" +
-    input.text.trim();
-  const messages = [{ role: "user", content: content }];
-  const { data } = await openai.post("chat/completions", {
-    model: model,
-    messages,
-  });
+function prepareResponse(data) {
   const response = data.choices[0].message.content.trim();
   // if holding shift, copy just the response. else, paste the last input and response.
   if (popclip.modifiers.shift) {
@@ -54,81 +10,110 @@ async function rewrite(input, options) {
   } else {
     popclip.pasteText(response);
   }
-  return null;
+}
+
+async function callOpenAPI(prompt, options) {
+  const key = options.apikey.trim();
+  if (!key || key.length === 0)
+    throw new Error("Settings error: missing OpenAPI key");
+
+  openai.defaults.headers.common.Authorization = `Bearer ${key}`;
+  const messages = [{ role: "user", content: prompt }];
+  const { data } = await openai.post("chat/completions", {
+    model: options.model,
+    messages,
+  });
+  return data;
+}
+
+async function spellingAndGrammar(input, options) {
+  const prompt =
+    "I want you to act as an English spelling corrector, only reply the correction, the improvements and nothing else, do not write explanations. improve the following sentence using BBC english: \n\n" +
+    input.text.trim();
+
+  const data = await callOpenAPI(prompt, options);
+
+  prepareResponse(data);
+}
+
+async function spellingAndGrammar(input, options) {
+  const prompt =
+    "I want you to act as an English spelling corrector, only reply the correction, the improvements and nothing else, do not write explanations. improve the following sentence using BBC english: \n\n" +
+    input.text.trim();
+
+  const data = await callOpenAPI(prompt, options);
+  prepareResponse(data);
 }
 
 async function summarize(input, options) {
-  openai.defaults.headers.common.Authorization = `Bearer ${options.apikey}`;
-  const content =
+  const prompt =
     "You are a highly skilled AI trained in language comprehension and summarization. I would like you to read the following text and summarize it into a concise abstract paragraph. Aim to retain the most important points, providing a coherent and readable summary that could help a person understand the main points of the discussion without needing to read the entire text. avoid unnecessary details or tangential points: \n\n" +
     input.text.trim();
-  const messages = [{ role: "user", content: content }];
-  const { data } = await openai.post("chat/completions", {
-    model: model,
-    messages,
-  });
+
+  const data = await callOpenAPI(prompt, options);
+  return data.choices[0].message.content.trim();
+}
+
+async function makeLonger(input, options) {
+  const prompt =
+    "I'll give you text. You'll rewrite it and output it longer Keep the meaning the same as well as the language. Only give me the output and nothing else. Now, using the concepts above, re-write the following text. Respond in the same language variety or dialect of the following text: \n\n" +
+    input.text.trim();
+
+  const data = await callOpenAPI(prompt, options);
+  return data.choices[0].message.content.trim();
+}
+
+async function makeShorter(input, options) {
+  const prompt =
+    "I'll give you text. You'll rewrite it and output it shorter to be no more than half the number of characters of the original text.Keep the meaning the same. Only give me the output and nothing else.Now, using the concepts above, re-write the following text. Respond in the same language variety or dialect of the following text: \n\n" +
+    input.text.trim();
+
+  const data = await callOpenAPI(prompt, options);
   return data.choices[0].message.content.trim();
 }
 
 async function correctFrench(input, options) {
-  openai.defaults.headers.common.Authorization = `Bearer ${options.apikey}`;
-  const content =
+  const prompt =
     "Corrige mon Francais, je veux simplement une reponse, ne rajoute rien: \n\n" +
     input.text.trim();
-  const messages = [{ role: "user", content: content }];
-  const { data } = await openai.post("chat/completions", {
-    model: model,
-    messages,
-  });
-  const response = data.choices[0].message.content.trim();
-  // if holding shift, copy just the response. else, paste the last input and response.
-  if (popclip.modifiers.shift) {
-    popclip.copyText(response);
-  } else {
-    popclip.pasteText(response);
-  }
-  return null;
+
+  const data = await callOpenAPI(prompt, options);
+  prepareResponse(data);
 }
 
-async function improveText(input, options) {
-  openai.defaults.headers.common.Authorization = `Bearer ${options.apikey}`;
-  const content =
-    "I want you to act as an English spelling corrector and improver. Keep the meaning the same, but make them more business friendly, avoid complex words and verbs, use BBC english. I want you to only reply the correction, the improvements and nothing else, do not write explanations. Correct and improve the following sentence: \n\n" +
+async function improveWritting(input, options) {
+  const prompt =
+    "I want you to act as an English spelling corrector and improver. Keep the meaning the same, use a " +
+    options.tone +
+    " tone, avoid complex words and verbs. I want you to only reply the correction, the improvements and nothing else, do not write explanations. Correct and improve the following sentence: \n\n" +
     input.text.trim();
-  const messages = [{ role: "user", content: content }];
-  const { data } = await openai.post("chat/completions", {
-    model: model,
-    messages,
-  });
 
-  const response = data.choices[0].message.content.trim();
-  // if holding shift, copy just the response. else, paste the last input and response.
-  if (popclip.modifiers.shift) {
-    popclip.copyText(response);
-  } else {
-    popclip.pasteText(response);
-  }
-  return null;
+  const data = await callOpenAPI(prompt, options);
+
+  prepareResponse(data);
 }
 
 exports.actions = [
-  // {
-  //   title: "Execute the selected prompt",
-  //   // after: "paste-result",
-  //   code: prompt,
-  //   icon: "./prompt.svg",
-  // },
   {
-    title: "Improve the text",
-    //after: "copy-result",
-    code: improveText,
-    icon: "symbol:pencil.and.outline",
+    title: "Improve Writing",
+    code: improveWritting,
+    icon: "iconify:tabler:file-text-ai",
   },
   {
-    title: "Correct the text",
-    after: "copy-result",
-    code: rewrite,
-    icon: "./spell-check.svg",
+    title: "Correct Spelling&Grammar",
+    // after: "copy-result",
+    code: spellingAndGrammar,
+    icon: "iconify:ic:round-fact-check",
+  },
+  {
+    title: "Make Longer",
+    code: makeLonger,
+    icon: "iconify:mdi:file-plus",
+  },
+  {
+    title: "Make Shorter",
+    code: makeShorter,
+    icon: "iconify:mdi:file-minus",
   },
   {
     title: "Summarize the text",
@@ -137,7 +122,7 @@ exports.actions = [
     icon: "symbol:arrow.down.right.and.arrow.up.left",
   },
   {
-    title: "Corrige mon francais",
+    title: "Corrige mon Français",
     //after: "copy-result",
     code: correctFrench,
     icon: "./FRA.svg",
